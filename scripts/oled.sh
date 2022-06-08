df0e6e7460ba3022a51872710f784af115af9f29
supdrewin <supdrewin@outlook.com>
2022-06-08 19:04:16 +0800

    Retry after OLED failed

--- scripts/oled.sh
+++ scripts/oled.sh
@@ -3,11 +3,15 @@
 # oled.sh - written by supdrewin
 # Mon Jun 6 13:25:14 CST 2022
 
-# shellcheck disable=SC1091
-. "%{prefix}/lib/rgb-cooling-hat/config.sh"
+while :; do
+    # shellcheck disable=SC1091
+    . "%{prefix}/lib/rgb-cooling-hat/config.sh"
 
-# shellcheck disable=SC1091,SC2154
-. "$rgb_cooling_hat_config_path/config"
+    # shellcheck disable=SC1091,SC2154
+    . "$rgb_cooling_hat_config_path/config"
 
-# shellcheck disable=SC2154
-"%{prefix}/lib/rgb-cooling-hat/ssd1306" "$device" "$thermal"
+    # shellcheck disable=SC2154
+    "%{prefix}/lib/rgb-cooling-hat/ssd1306" "$device" "$thermal"
+
+    sleep 1
+done
